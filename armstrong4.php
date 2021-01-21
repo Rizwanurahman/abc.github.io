e3529b167a969224a87dc8bf159359b2f88fce1a
Rizwanurahman <62349722+Rizwanurahman@users.noreply.github.com>
2021-01-21 21:41:23 +0530

    Update armstrong4.php

--- armstrong4.php
+++ armstrong4.php
@@ -9,20 +9,16 @@
 </html>  
 <?php  
  if($_POST)  
- {   
-  //get the number entered  
+ {    
   $number = $_POST['number'];  
-  //store entered number in a variable  
   $a = $number;  
   $sum  = 0;  
-  //run loop till the quotient is 0  
   while( $a != 0 )  
   {  
-   $rem   = $a % 10; //find reminder  
-   $sum   = $sum + ( $rem * $rem * $rem ); //cube the reminder and add it to the sum variable till the loop ends  
-   $a   = $a / 10; //find quotient. if 0 then loop again  
+   $rem   = $a % 10;
+   $sum   = $sum + ( $rem * $rem * $rem );  
+   $a   = $a / 10; 
   }  
-  //if the entered number and $sum value matches then it is an armstrong number  
   if( $number == $sum )  
   {  
    echo "$number an Armstrong Number";  
